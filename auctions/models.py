e2d33af54681c95d604e07b25e30f2d8c80068ab
OlegAnts <93344926+OlegAnts@users.noreply.github.com>
2023-02-21 22:26:45 +0300

    Create Listing: Users should be able to visit a page to create a new listing. They should be able to specify a title for the listing, a text-based description, and what the starting bid should be. Users should also optionally be able to provide a URL for an image for the listing and/or a category (e.g. Fashion, Toys, Electronics, Home, etc.).

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,27 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    CATEGORIES = [
+        ("NC", "No Category"),
+        ("TS", "Toys"),
+        ("FN", "Fashion"),
+        ("EL", "Electronics")
+    ]
+
+    list_title = models.CharField(max_length=20, null=True)
+    list_description = models.TextField(max_length=100, default='Empty description')
+    list_category = models.CharField(max_length=20, choices=CATEGORIES, default="No Category")
+
+    def __str__(self):
+        return f"{self.list_title}"
+
+
+# class Bid(models.Model):
+#     pass
+
+
+# class Comment(models.Model):
+#     pass
