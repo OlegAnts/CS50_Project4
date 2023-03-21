4052b6a69ab3f3732707e2dacbc11f2c68433d6a
OlegAnts <93344926+OlegAnts@users.noreply.github.com>
2023-03-21 23:46:22 +0300

    Add Bid model

--- auctions/models.py
+++ auctions/models.py
@@ -1,3 +1,4 @@
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -19,18 +20,23 @@ class Listing(models.Model):
     list_description = models.TextField(max_length=100, default='Empty description')
     list_category = models.CharField(max_length=20, choices=CATEGORIES, default="No Category")
     starting_bid = models.DecimalField(max_digits=6, decimal_places=2, default='1.00')
-
     list_image_URL = models.URLField(default="https://www.tharagold.in/assets/img/no-product-found.png")
 
-    list_owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    list_owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="my_listings")
+    list_buyers = models.ManyToManyField(User, through="Bid", related_name="lots")
 
 
     def __str__(self):
         return f"{self.list_title}"
 
 
-# class Bid(models.Model):
-#     pass
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, null=True)
+    bid_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.user}: {self.bid_price}$"
 
 
 # class Comment(models.Model):
